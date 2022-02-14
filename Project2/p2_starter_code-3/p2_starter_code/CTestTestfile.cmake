0076f073e2739acb7ec8bc09bfd7a7da94ccda63
Zeke Mariotti <eam180@pitt.edu>
2022-02-14 09:16:02 -0500

    Project2 Start

---
+++ Project2/p2_starter_code-3/p2_starter_code/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/zeke/Desktop/ECE302/ECE-302/Project2/p2_starter_code-3/p2_starter_code
+# Build directory: /home/zeke/Desktop/ECE302/ECE-302/Project2/p2_starter_code-3/p2_starter_code
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unittest "FindPalindrome-tests")
+set_tests_properties(unittest PROPERTIES  _BACKTRACE_TRIPLES "/home/zeke/Desktop/ECE302/ECE-302/Project2/p2_starter_code-3/p2_starter_code/CMakeLists.txt;18;add_test;/home/zeke/Desktop/ECE302/ECE-302/Project2/p2_starter_code-3/p2_starter_code/CMakeLists.txt;0;")
