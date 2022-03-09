c668ff13f1caad51ac94579703fbf73669af185c
Zeke Mariotti <eam180@pitt.edu>
2022-03-09 14:24:45 -0500

    Project 3 No Changes, compiles

---
+++ Project3/p3_starter_code/p3_starter_code/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/zeke/Desktop/ece0302/ECE-302/Project3/p3_starter_code/p3_starter_code
+# Build directory: /home/zeke/Desktop/ece0302/ECE-302/Project3/p3_starter_code/p3_starter_code
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unittest "XMLParser-tests")
+set_tests_properties(unittest PROPERTIES  _BACKTRACE_TRIPLES "/home/zeke/Desktop/ece0302/ECE-302/Project3/p3_starter_code/p3_starter_code/CMakeLists.txt;22;add_test;/home/zeke/Desktop/ece0302/ECE-302/Project3/p3_starter_code/p3_starter_code/CMakeLists.txt;0;")
