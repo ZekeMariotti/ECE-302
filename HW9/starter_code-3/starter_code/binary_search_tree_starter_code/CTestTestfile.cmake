1da35e4ecdcc8ca3748182dd0ed6e736f9178805
Zeke Mariotti <eam180@pitt.edu>
2022-03-31 13:29:16 -0400

    HW9 and Project 4

---
+++ HW9/starter_code-3/starter_code/binary_search_tree_starter_code/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/zeke/Desktop/ECE302/ECE-302/HW9/starter_code-3/starter_code/binary_search_tree_starter_code
+# Build directory: /home/zeke/Desktop/ECE302/ECE-302/HW9/starter_code-3/starter_code/binary_search_tree_starter_code
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_bst "test_bst")
+set_tests_properties(test_bst PROPERTIES  _BACKTRACE_TRIPLES "/home/zeke/Desktop/ECE302/ECE-302/HW9/starter_code-3/starter_code/binary_search_tree_starter_code/CMakeLists.txt;11;add_test;/home/zeke/Desktop/ECE302/ECE-302/HW9/starter_code-3/starter_code/binary_search_tree_starter_code/CMakeLists.txt;0;")
--- HW9/starter_code-3/starter_code/binary_tree_starter_code/test.cpp
+++ HW9/starter_code-3/starter_code/binary_tree_starter_code/test.cpp
@@ -15,11 +15,26 @@ int main(int argc, char** argv)
     BinaryTree<ItemType, FunctionType> T2("C");
 
     BinaryTree<ItemType, FunctionType> T3("A");
+    BinaryTree<ItemType, FunctionType> T4("D");
+    BinaryTree<ItemType, FunctionType> T5("E");
+    BinaryTree<ItemType, FunctionType> T6("F");
+    BinaryTree<ItemType, FunctionType> T7("G");
+    
+    T1.attachLeftSubtree(T4);
+    T1.attachRightSubtree(T5);
+
+    T2.attachLeftSubtree(T6);
+    T2.attachRightSubtree(T7);
+
     T3.attachLeftSubtree(T1);
     T3.attachRightSubtree(T2);
+
     T3.postorderTraverse(&PrintNode);
+    cout << endl;
     T3.preorderTraverse(&PrintNode);
+    cout << endl;
     T3.inorderTraverse(&PrintNode);
+    cout << endl;
 
     // now T1 and T2 are no longer trees, but empty objects
 
